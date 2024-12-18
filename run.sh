09e1a21a6d7b059514c195be0fcd6edd553d744d
Oliver Rice <oliverrice1998@gmail.com>
2024-12-18 14:49:15 +0000

    Improved makefile and bash

--- run.sh
+++ run.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-for i in {0..7}
-    do
-    echo run.py $i 4
-    python run.py $i 4
-    done
-echo All done
+# Request resources:
+#SBATCH -n 16       # 1 CPU core
+#SBATCH --mem=8G      # 1 GB RAM
+#SBATCH --time=0-48:0:0  # 1 hour (days-hours:minutes:seconds)
+#SBATCH -p shared
+#SBATCH --array=10-17
+
+# Make python available:
+module load python
+module load gcc
+module load openmpi
+module load netcdf
+
+pip install numpy
+
+# Commands to be run:
+
+echo "Task number $SLURM_ARRAY_TASK_ID"
+
+python run.py ${SLURM_ARRAY_TASK_ID}
+
+mpirun -np 16 ./bin/mf3d ${SLURM_ARRAY_TASK_ID}
 
